ac8b01eebfaf968ac42b7eb56e38b4646b557a51
Rodrigo <rodrigo.villamil@gmail.com>
2017-05-20 11:38:08 +0200

    Subiendo cambios a lo loco al repositorio

--- vws-resources/src/main/java/es/rvp/web/vws/SwaggerConfig.java
+++ vws-resources/src/main/java/es/rvp/web/vws/SwaggerConfig.java
@@ -14,24 +14,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("es.rvp.web.vws.resources.controllers"))
-                .paths(PathSelectors.any())
-                .build().apiInfo(this.metaData());
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select().apis(RequestHandlerSelectors.basePackage("es.rvp.web.vws.resources.controllers"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(this.metaData());
 
-    }
-
-    private ApiInfo metaData() {
-        return new ApiInfo(
-                "VWS REST API",
-                "REST API for Video websites scraper",
-                "1.0",
-                "Terms of service",
-                new Contact("Rodrigo Villamil",
-                			"https://github.com/rvillamil/", "rodrigo.villamil@gmail.com"),
-                			"GNU General Public License v3.0",
-                			"https://www.gnu.org/licenses/gpl-3.0.en.html");
-    }
+	}
+	// Visit: http://localhost:8080/swagger-ui.html
+	private ApiInfo metaData() {
+		return new ApiInfo(
+				"VWS REST API",
+				"REST API for Video websites scraper",
+				"1.0",
+				"Terms of service",
+				new Contact("Rodrigo Villamil",
+						"https://github.com/rvillamil/", "rodrigo.villamil@gmail.com"),
+				"GNU General Public License v3.0",
+				"https://www.gnu.org/licenses/gpl-3.0.en.html");
+	}
 }
--- vws-ui/assets/js/main.js
+++ vws-ui/assets/js/main.js
@@ -1,29 +1,70 @@
 /**
  * Javascript functions
+ *
+ * https://www.w3schools.com/js/js_ajax_http.asp
  */
 
 /**
  * Use: loadShows("url");
  */
-function loadShows(urlPath) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
+function loadShows(urlPath, cFunction) {
+	var request = new XMLHttpRequest();
+	// onreadystatechange: Defines a function to be called when the readyState
+	// property changes
+	request.onreadystatechange = function() {
+		// readyState:
+		// - 0: request not initialized
+		// - 1: server connection established
+		// - 2: request received
+		// - 3: processing request
+		// - 4 request finished and response is ready
+		//
+		// status:
+		// - 200 : OK ... https://www.w3schools.com/tags/ref_httpmessages.asp
+		//
+		// statusText:
+		// - "OK", "not Found" ..Returns the status-text
+		if (this.readyState == 0 ) {
+			console.log ("WTF! Server not running?")
+		}
+
+		/*
+		if (this.readyState == 3 ) {
+			console.log ("Processing ..")
+		}
+		*/
+
 		if (this.readyState == 4 && this.status == 200) {
-			showList = JSON.parse(this.responseText);
-
-			for (show in showList) {
-				//document.getElementById("show-list").innerHTML = "<li class=\"show-item\">"
-				document.getElementById("films").innerHTML += show.name
-			}
-
-		} else {
-			document.getElementById("films").innerHTML = "WTF!"
+			onSuccess(this);
 		}
 	};
-	xhttp.open("GET", "http://localhost:8080"+urlPath, true);
-	xhttp.send();
+
+	request.onerror = function() {
+		console.log("Error");
+	};
+
+	request.open("GET", "http://localhost:8080" + urlPath, true);
+	request.send();
 }
 
-function loadBillboardfilms(){
-	loadShows('/billboardfilms')
+function onSuccess(request) {
+	var htmlFragment = document.getElementById("films");
+	try {
+		var shows = JSON.parse(request.responseText);
+		var html  = "<ul>";
+		for (var i = 0; i < shows.length; i++) {
+			html += '<li><a href="' + shows[i]["urltodownload"] + '">'
+					+ shows[i]["title"] + "</a></li>";
+		}
+		html += "</ul>";
+		htmlFragment.innerHTML = html
+
+	} catch (err) {
+		console.log(err.message + " in " + request.responseText);
+		return;
+	}
+}
+
+function loadBillboardfilms() {
+	loadShows('/billboardfilms', onSuccess);
 }
