6f47719ace0736b843d83f8d0d62af326b38f371
Rodrigo <rodrigo.villamil@gmail.com>
2017-05-27 11:53:08 +0200

    Subiendo cambios a lo loco al repositorio

--- vws-ui/assets/js/main.js
+++ vws-ui/assets/js/main.js
@@ -5,9 +5,41 @@
  */
 
 /**
+ * Replace de tabcontent with name 'showType' with HTML show list
+ *
+ * @param evt
+ * @param showType:
+ *            tabcontent_billboardfilms, tabcontent_videopremieres,..
+ */
+function openShows(evt, showType) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(showType).style.display = "block";
+	evt.currentTarget.className += " active";
+
+	if (showType == "tabcontent_billboardfilms") {
+		console.log("Getting billboard films async mode ...")
+		loadShows('/billboardfilms', onSuccess, showType);
+
+	} else if (showType == "tabcontent_videopremieres") {
+		console.log("Getting video premieres async mode ...")
+		loadShows('/videopremieres', onSuccess, showType);
+	} else {
+		setTextStatusBar("ERROR!! 'tabconten' not exists" + showType)
+	}
+}
+
+/**
  * Use: loadShows("url", "callback function");
  */
-function loadShows(urlPath, cFunction) {
+function loadShows(urlPath, cFunction, showType) {
 	var request = new XMLHttpRequest();
 	// onreadystatechange: Defines a function to be called when the readyState
 	// property changes
@@ -24,8 +56,8 @@ function loadShows(urlPath, cFunction) {
 		//
 		// statusText:
 		// - "OK", "not Found" ..Returns the status-text
-		if (this.readyState == 0 ) {
-			console.log ("WTF! Server not running?")
+		if (this.readyState == 0) {
+			setTextStatusBar("WTF! Server not running?");
 		}
 
 		/*
@@ -33,80 +65,90 @@ function loadShows(urlPath, cFunction) {
 		 */
 
 		if (this.readyState == 4 && this.status == 200) {
-			onSuccess(this);
+			onSuccess(this, showType);
 		}
 	};
 
 	request.onerror = function() {
-		document.getElementById("shows-container").innerHTML = "Errro: " + this.readyState + " -" + this.status + "-" + this.statusText;
-		console.log("Error");
-
+		setTextStatusBar("Error --> readyState: " + this.readyState
+				+ " - status: " + this.status + " - statusText "
+				+ this.statusText);
 	};
 
 	request.open("GET", "http://localhost:8080" + urlPath, true);
 	request.send();
 }
 
-
-/*
-SHOW:
-"title": "string", "urltodownload": "string",  "urlwithCover": "string"
-"session": "string", "episode": "string",
-"releaseDate": "string", "quality": "string", "fileSize": "string"
-"filmaffinityPoints": 0,
-"description": "string",
-"sinopsis": "string",
-*/
-function onSuccess(request) {
-	var htmlFragment = document.getElementById("shows-container");
+/**
+ * On request success event, then create new show list in the 'show-container'
+ * div tag
+ *
+ * @param response
+ *            JSON with the show list
+ * @param showType
+ *            tabcontent_billboardfilms, tabcontent_videopremieres or
+ *            tabcontent_tvshows
+ *
+ * @returns nothing
+ */
+function onSuccess(response, showType) {
 	try {
-		var shows = JSON.parse(request.responseText);
+		var newHtml = "";
+		var shows = JSON.parse(response.responseText);
 
-		var newHtml  ="";
 		for (var i = 0; i < shows.length; i++) {
 			console.log("Processing show " + shows[i]['title'])
 			newHtml += "<div class='show-container'>";
 
 			// Filmaffinity Points
-			if (shows[i]["filmaffinityPoints"] != null){
-				newHtml += 	 "<div class='show-box-text'>" + " Filmaffinity "+ shows[i]["filmaffinityPoints"] + "</div>";
+			if (shows[i]["filmaffinityPoints"] != null) {
+				newHtml += "<div class='show-box-text'>" + " Filmaffinity "
+						+ shows[i]["filmaffinityPoints"] + "</div>";
 			}
 			// Cover
-			newHtml +=   "<div class='show-box-img'>";
-			newHtml += 	    "<a href='"+ shows[i]["urltodownload"] + "'>";
-			newHtml += 	        "<img src='" + shows[i]["urlwithCover"] + "'" + " alt='cover' " + "/>";
-			newHtml +=       "</a>";
-			newHtml +=       "<span class='tooltiptext'>" + shows[i]["title"] + "</span>";
-			newHtml +=   "</div>";
+			newHtml += "<div class='show-box-img'>";
+			newHtml += "<a href='" + shows[i]["urltodownload"] + "'>";
+			newHtml += "<img src='" + shows[i]["urlwithCover"] + "'"
+					+ " alt='cover' " + "/>";
+			newHtml += "</a>";
+			newHtml += "<span class='tooltiptext'>" + shows[i]["title"]
+					+ "</span>";
+			newHtml += "</div>";
 
 			// Title
-			newHtml += 	  "<div class='show-box-title'>" + shows[i]["title"] + "</div>";
+			newHtml += "<div class='show-box-title'>" + shows[i]["title"]
+					+ "</div>";
 
 			// Session and Episode
-			if (shows[i]["session"] != null){
-				newHtml +=    "<div class='show-box-text'>" + "Temporada " + shows[i]["session"] + "-" + "Episodio " + shows[i]["episode"] + "</div>";
+			if (shows[i]["session"] != null) {
+				newHtml += "<div class='show-box-text'>" + "Temporada "
+						+ shows[i]["session"] + "-" + "Episodio "
+						+ shows[i]["episode"] + "</div>";
 			}
 			// Quality
-			newHtml += 	  "<div class='show-box-text'>" + shows[i]["quality"] + "</div>";
+			newHtml += "<div class='show-box-text'>" + shows[i]["quality"]
+					+ "</div>";
 			// Releasedate and filesize
-			newHtml += 	  "<div class='show-box-text'>" + shows[i]["releaseDate"] + " - " + shows[i]["fileSize"] + "</div>";
+			newHtml += "<div class='show-box-text'>" + shows[i]["releaseDate"]
+					+ " - " + shows[i]["fileSize"] + "</div>";
 
 			newHtml += "</div>";
-		}
 
-		console.log (newHtml)
-
-		htmlFragment.innerHTML = newHtml;
-	} catch (err) {
-		console.log(err.message + " in " + request.responseText);
+		} // End for
+		setHTMLInTabContainer(showType, newHtml);
+	} // End try
+	catch (err) {
+		setTextStatusBar(err.message + " in " + response.responseText);
 		return;
 	}
+	// console.log("newhtml with shows: " + newHtml)
 }
 
-function loadBillboardfilms() {
-	loadShows('/billboardfilms', onSuccess);
+function setTextStatusBar(text) {
+	console.log("Status Bar text: " + text);
+	document.getElementById('statusbar').innerHTML = text;
 }
 
-function loadVideoPremieres() {
-	loadShows('/videopremieres', onSuccess);
+function setHTMLInTabContainer(tabContainer, htmlFragment) {
+	document.getElementById(tabContainer).innerHTML = htmlFragment;
 }
