252579f99cb20745139d824bf2b00f1e9ed1e1c5
Rodrigo Villamil <rodrigo.villamil@diagroup.com>
2017-07-04 14:27:34 +0200

    Subiendo cambios a lo loco al repositorio

--- vws-persistence/docker-build.sh
+++ vws-persistence/docker-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 #
 # Copyright (C) Rodrigo Villamil Perez 2017
-# Fichero: build-images.sh
+# Fichero: docker-build.sh
 # Autor: rvp001es
 # Fecha: 26/06/17
 #
 #
 cmd="mvn clean install -Dmaven.test.skip=true -P docker-support"
-echo "Running maven.. SKIPPING Test: ${cmd}"
+echo "Buildind docker containers. Running maven.. SKIPPING Test: ${cmd}"
 ${cmd}
