95493a71fad659acefa83ee1bea176241a5819da
Rodrigo Villamil <rodrigo.villamil@diagroup.com>
2017-09-27 12:11:36 +0200

    comentarios

--- vws-resources/src/main/java/es/rvp/web/vws/resources/controllers/PiracyController.java
+++ vws-resources/src/main/java/es/rvp/web/vws/resources/controllers/PiracyController.java
@@ -35,11 +35,12 @@ public class PiracyController {
 	 */
 
 	/*
-	 TODO 00: PMV Produccion: Autenticacion basica, usuario y password en BB.DD 
-	  - Revisar el proyecto en Github: https://github.com/spring-guides/tut-bookmarks	  
+	 TODO 00: PMV Produccion: Autenticacion basica, usuario y password en BB.DD
+	  - Con JWT para la autenticación: https://www.adictosaltrabajo.com/tutoriales/securizar-un-api-rest-utilizando-json-web-tokens/
+	  - Revisar el proyecto en Github: https://github.com/spring-guides/tut-bookmarks
 	  		https://spring.io/guides/tutorials/bookmarks/#_securing_a_rest_service
 	    Montar un servidor Oauth y hacer pruebas para entender el Outhz ¿Como damos permisos en Oauth a Olga y Rodrigo y al resto no?
-	 
+
 	 */
 
 	/*
@@ -47,7 +48,7 @@ public class PiracyController {
 	  - Finalizar el resto de Test unitarios para los controladores REST, persistencia..todo lo que falte
 	    	   http://www.baeldung.com/spring-boot-testing?utm_content=buffer61c1e&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer
 	  - Actualizar el README correctamente: https://www.genbetadev.com/software-libre-y-licencias/checklist-para-liberar-un-proyecto-open-source-en-github
-	*/
+	 */
 
 	/*
 	 TODO: Autenticacion/Autorizacion: Autorizar a la aplicacion UI a consumir servicios de mi API de varias formas
@@ -60,33 +61,33 @@ public class PiracyController {
 
 	/*
 	 TODO: Spring Cloud, Microservicios, Eureka: https://spring.io/blog/2015/07/14/microservices-with-spring
-	*/
+	 */
 
-    /*
+	/*
 	 TODO: Revision del uso de PUT,POST e idempotencia. ¿Como evitamos problemas de concurrencia? "Optimistic lock"
 	  - Charla Gus: https://youtu.be/fZo8Zp2otqQ
 	  - http://labs.unacast.com/2016/02/25/on-idempotency-in-distributed-rest-apis/
 	  - https://spring.io/guides/tutorials/bookmarks/
 	  - Best practices for concurrency control in REST APIs: https://goo.gl/Xqqvii
 	  - https://stackoverflow.com/questions/30080634/concurrency-in-a-rest-api
-    */
+	 */
 
 	/*
 	 TODO: Revisar la configuracion de spring boot y la carga de properties mas interesantes
       - https://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html
       - http://www.baeldung.com/spring-boot-application-configuration
-	*/
+	 */
 
 	/*
 	 TODO: Redis - PoC
 	  - Introduction to Spring Data Redis - https://goo.gl/oegRqu
 	  - Introducción a la base de datos NoSQL Redis - https://goo.gl/JBqiHE
-	*/
+	 */
 
 	/*
 	 TODO: Spring boot actuator, revision
 	  - http://www.baeldung.com/spring-boot-actuators?utm_content=buffer309af&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer
-	*/
+	 */
 
 	/*
 	 TODO: Jenkins CI and Docker - Finalizar el soporte para Docker, de la siguiente forma:
@@ -96,7 +97,7 @@ public class PiracyController {
 	       docker.build("arungupta/docker-jenkins-pipeline:${env.BUILD_NUMBER}")
 	    }
 	   }
-	*/
+	 */
 
 	/*
 	 TODO: Funcionalidades de negocio
@@ -104,15 +105,15 @@ public class PiracyController {
 	    Spiderman salga y ademas en la calidad que pongamos, la pondrá a descargar.
 
 	  - Poner las notas de las pelis:
-	       * Implementar el parser de filmaffinity o bien,
-	       * http://www.cinesift.com/
-	       * Usar una API pública de metracritic o similar ( https://www.publicapis.com/ )
-	*/
+	 * Implementar el parser de filmaffinity o bien,
+	 * http://www.cinesift.com/
+	 * Usar una API pública de metracritic o similar ( https://www.publicapis.com/ )
+	 */
 
 	/*
 	 TODO: Revisar la configuracion del apache y el tomcat embebidos
 	  - https://elpesodeloslunes.wordpress.com/2014/09/07/el-servidor-tomcat-desde-cero-3-configuracion-basica/
-	*/
+	 */
 
 
 	/*
@@ -147,7 +148,7 @@ public class PiracyController {
 	 * @return One text if this web site is available
 	 */
 	@RequestMapping(value="/",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	String hello() {
 		LOGGER.info(WELCOME_TEXT);
 		return WELCOME_TEXT;
@@ -159,16 +160,16 @@ public class PiracyController {
 	 * @return JSon object, with the billboard films in the torrent portal [0, maxBillboardFilms]
 	 */
 	@RequestMapping(value="/billboardfilms/",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	public ResponseEntity<?> parseBillBoardFilms() {
 		LOGGER.info("PiracyController - Getting billboard films ...");
-		Set<Show> shows = this.webTorrentSpider.parseBillboardFilms(this.maxBillboardFilms);
+		final Set<Show> shows = webTorrentSpider.parseBillboardFilms(maxBillboardFilms);
 		if ( shows.isEmpty()) {
 			LOGGER.warn("PiracyController - Billbaord films not found.");
-            return new ResponseEntity<>(new CustomErrorType(
-            		"Billboard films list is empty"), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(shows, HttpStatus.OK);
+			return new ResponseEntity<>(new CustomErrorType(
+					"Billboard films list is empty"), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(shows, HttpStatus.OK);
 	}
 
 	/**
@@ -177,17 +178,17 @@ public class PiracyController {
 	 * @return JSon object, with the video premieres in the torrent portal [0, maxSize]
 	 */
 	@RequestMapping(value="/videopremieres/",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	public ResponseEntity<?> parseVideoPremieres() {
 		LOGGER.info("PiracyController - Getting video premieres ...");
-		Set<Show> shows = this.webTorrentSpider.parseVideoPremieres(this.maxVideoPremieres);
+		final Set<Show> shows = webTorrentSpider.parseVideoPremieres(maxVideoPremieres);
 		if ( shows.isEmpty()) {
 			LOGGER.warn("PiracyController - Video premieres not found.");
 
-            return new ResponseEntity<>(new CustomErrorType(
-            		"Video premieres list is empty"), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(shows, HttpStatus.OK);
+			return new ResponseEntity<>(new CustomErrorType(
+					"Video premieres list is empty"), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(shows, HttpStatus.OK);
 	}
 
 	/**
@@ -199,18 +200,18 @@ public class PiracyController {
 	 * @return JSon object, with last episodes from TV show between '0 and maxTVshows'
 	 */
 	@RequestMapping(value="/tvshows/{title}",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	public ResponseEntity<?> parseTVShow(@PathVariable("title") final String title) {
 		LOGGER.info("PiracyController - Getting the tvshow '{}'", title);
-		Set<Show> shows =  this.webTorrentSpider.parseTVShow(title, this.maxTVshows);
+		final Set<Show> shows =  webTorrentSpider.parseTVShow(title, maxTVshows);
 
 		if ( shows.isEmpty()) {
 			LOGGER.warn("PiracyController - TVShows episodes with title '{}' not found.", title);
 
-            return new ResponseEntity<>(new CustomErrorType(
-            		"TVShows list is empty"), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(shows, HttpStatus.OK);
+			return new ResponseEntity<>(new CustomErrorType(
+					"TVShows list is empty"), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(shows, HttpStatus.OK);
 	}
 
 }
