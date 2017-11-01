c55c22acad12cd776b248e1a00981dc44d0de421
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2017-11-01 19:42:54 +0100

    Comenzamos el refactoring a una estructura basada en componentes https://www.reaktor.com/blog/organizing-frontend-code-of-a-single-page-app

--- vws-ui/src/main/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Global config
- */
-var server = "http://localhost:8080";
-var url_base = "/api";
-var url_base_billboard_films = url_base + "/billboardfilms/";
-var url_base_video_premieres = url_base + "/videopremieres/";
-var url_base_tvshows = url_base + "/tvshows/";
-var url_base_favorites = url_base + "/favorites/";
