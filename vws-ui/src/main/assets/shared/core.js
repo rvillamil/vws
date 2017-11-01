14d2a39d36c7904cba802dc8d95c15825a6746b7
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2017-11-01 21:21:53 +0100

    Refactorizacion completada

---
+++ vws-ui/src/main/assets/shared/core.js
@@ -0,0 +1,53 @@
+/**
+ * Show javascrip 'alert' window
+ * @param {*} text The text to show
+ */
+function showAlertWindow(text) {
+    console.log("showAlertWindow: '" + text + "'");
+    alert(text);
+}
+
+/**
+ * doPost function 
+ * 
+ * @param {*} url Target url
+ * @param {*} body JSon with body
+ * @param {*} JWToken Authorization token
+ * @param {*} onSuccess HTTP 200
+ * @param {*} onSuccessNewResource  HTTP 201
+ * @param {*} onError HTTP Error
+ */
+function doPost(url, body, JWToken, onSuccess, onSuccessNewResource, onError) {
+    console.log("doPost on '" + server + url + "' whith body '" + body + "'");
+    var request = new XMLHttpRequest();
+    request.withCredentials = true;
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            if (onSuccess != null) {
+                onSuccess(this);
+            }
+        } else if (request.readyState === 4 && request.status === 201) {
+            console.log("doPost : 201 Resource created successfully");
+            if (onSuccessNewResource != null) {
+                onSuccessNewResource(this);
+            }
+        } else if (this.readyState == 4 && request.status === 0) {
+            showAlertWindow("ERROR!!! Backend down?");
+        } else if (this.readyState == 4 && request.status === 401) {
+            console.log("doPost : 401 Authorization error!");
+            showAlertWindow("Authorization error !!");
+        } else if (this.readyState == 4) {
+            console.log("doPost ERROR: [readyState: " + this.readyState + ", status: " + this.status + ", statusText: '" + this.statusText + "']");
+            if (onError != null) {
+                onError(this);
+            }
+        }
+    };
+
+    request.open("POST", server + url, true);
+    if (JWToken != null) {
+        request.setRequestHeader("Authorization", JWToken);
+    }
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(body);
+}
